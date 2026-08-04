8f16b7b19bf3a81fc591142c17a64b121890d04b
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-08-04 11:53:16 +0530

    Add

--- Unit_1/Week_1/DAY_1/1.js
+++ Unit_1/Week_1/DAY_1/1.js
@@ -1,19 +1,32 @@
-console.log("Hello World");
+// console.log("Hello World");
+// const { log } = require("console");
 
-const { log } = require("console");
 // Node js core modules 
 
 // 1. os- operating System 
+// const os = require("os");
+// console.log(os.freemem());
+// console.log(os.platform());
+// console.log(os.version());
+// console.log(os.homedir());
+// console.log(os.cpus());
 
-const os = require("os");
+// 2. dns
+// const dns = require("dns");
+// console.log(dns.getServers());
 
-console.log(os.freemem());
+// 3. path- address
+// const path = require("path");
+// console.log(path.resolve());
 
-console.log(os.platform());
-console.log(os.version());
-console.log(os.homedir());
-console.log(os.cpus());
-const dns = require("dns");
+// 4. crypto 
+// const { log } = require("console");
+const crypto = require("crypto");
 
-console.log(dns.getServers());
+// Hashing-
+var name = "Bhavya Anand";
+const hashedValue = crypto.createHash("sha256");
+hashedValue.update(name);
+const finalHash = hashedValue.digest("hex");
 
+console.log("Hashed output:", finalHash);
