7974bbab0b8a8cec0507b369bae6205d6562ed06
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-08-04 11:34:21 +0530

    Update README and Unit 1 files

---
+++ Unit_1/Week_1/DAY_1/1.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+const { log } = require("console");
+// Node js core modules 
+
+// 1. os- operating System 
+
+const os = require("os");
+
+console.log(os.freemem());
+
+console.log(os.platform());
+console.log(os.version());
+console.log(os.homedir());
+console.log(os.cpus());
+
+
