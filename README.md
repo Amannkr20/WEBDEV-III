ef4338371b41dae47848b7676e0ccb4d0d7ee686
Aman Kumar <amann.kr20@gmail.com>
2026-08-20 11:11:13 +0530

    Update complete Web Dev 3 folder

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # WEB_DEV_3
 WEB_DEV_3
 
-cd WEB_DEV_3
-git add .
-git commit -m "Add"
+
 git push origin main
+git status
+
--- Unit_2/Day_3/1.js
+++ Unit_2/Day_3/1.js
@@ -9,6 +9,8 @@ const express = require("express");
 const fs = require("fs");
 // S-2- Create app
 const app = express();
+// middleware- request body parser
+app.use(express.json());
 // S-3- Create api/routes
 app.get("/", (req, res) => {
   res.send({ msg: "Wlcome to my Website " });
@@ -21,14 +23,38 @@ app.get("/contact", (req, res) => {
 });
 
 // get API/Read Route
-app.get("/services", (req, res) => {
+app.get("/Read", (req, res) => {
   const data = fs.readFileSync("1.json", "utf-8");
   console.log(data);
   console.log(data.students);
 
   const jsData = JSON.parse(data);
   console.log(jsData);
-  res.send({ msg: "Services Page" });
+  res.send({ msg: "Read Page" });
+});
+// app.get("/stread", (req, res) => {
+//   const data = fs.readFileSync("1.json", "utf-8");
+//   console.log(data);
+//   console.log(data.students);
+
+//   const jsData = JSON.parse(data);
+//   console.log(jsData.students);
+//   res.send({ msg: "Services Page" });
+// });
+
+// Adding new data-
+app.post("/create", (req, res) => {
+  const payload = req.body;
+  console.log(payload);
+  const data = JSON.parse(fs.readFileSync("1.json", "utf-8"));
+  const stdata = data.students;
+  stdata.push(payload);
+  console.log(stdata);
+  data.students = stdata;
+
+  fs.writeFileSync("1.json", JSON.stringify(data));
+
+  res.send({ msg: "New Student created" });
 });
 // S-4- Run app
 app.listen(8080, () => {
